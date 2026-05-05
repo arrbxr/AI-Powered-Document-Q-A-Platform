31ef6ee4054e0fa2d98538bddba3eb7debd4ba36
arrbxr <arr.bxr@gmail.com>
2026-05-06 01:24:10 +0530

    Service discover added

---
+++ discovery-server/src/test/java/com/docqa/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docqa.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
